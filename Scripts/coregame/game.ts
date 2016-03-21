864d9be98e150a6413307db9775d97a96067c666
DectorArc <emmitStrong@gmail.com>
2016-03-21 01:34:12 -0400

    Bare Bones working example without movement

--- Scripts/coregame/game.ts
+++ Scripts/coregame/game.ts
@@ -1,13 +1,34 @@
 /// <reference path="_importedScripts.ts"/>
 
+// MAIN GAME FILE
+
+// THREEJS Aliases
 import Scene = Physijs.Scene;
 import Renderer = THREE.WebGLRenderer;
 import PerspectiveCamera = THREE.PerspectiveCamera;
-import Vector3 = THREE.Vector3;
-import CScreen = config.Screen;
-import CubeGeometry = THREE.CubeGeometry;
 import BoxGeometry = THREE.BoxGeometry;
+import CubeGeometry = THREE.CubeGeometry;
+import PlaneGeometry = THREE.PlaneGeometry;
+import SphereGeometry = THREE.SphereGeometry;
+import Geometry = THREE.Geometry;
+import AxisHelper = THREE.AxisHelper;
 import LambertMaterial = THREE.MeshLambertMaterial;
+import MeshBasicMaterial = THREE.MeshBasicMaterial;
+import Material = THREE.Material;
+import Mesh = THREE.Mesh;
+import Object3D = THREE.Object3D;
+import SpotLight = THREE.SpotLight;
+import PointLight = THREE.PointLight;
+import AmbientLight = THREE.AmbientLight;
+import Control = objects.Control;
+import GUI = dat.GUI;
+import Color = THREE.Color;
+import Vector3 = THREE.Vector3;
+import Face3 = THREE.Face3;
+import Point = objects.Point;
+import CScreen = config.Screen;
+import Clock = THREE.Clock;
+
 //Custom Game Objects
 import gameObject = objects.gameObject;
 
@@ -16,38 +37,85 @@ Physijs.scripts.worker = "/Scripts/lib/Physijs/physijs_worker.js";
 Physijs.scripts.ammo = "/Scripts/lib/Physijs/examples/js/ammo.js";
 
 
+// setup an IIFE structure (Immediately Invoked Function Expression)
 var game = (() => {
-    
-    var scene: Scene;
-    var camera: PerspectiveCamera;
+
+    // declare game objects
+    var havePointerLock: boolean;
+    var element: any;
+    var scene: Scene = new Scene(); // Instantiate Scene Object
     var renderer: Renderer;
+    var camera: PerspectiveCamera;
+    var control: Control;
+    var gui: GUI;
+    var stats: Stats;
+    var blocker: HTMLElement;
+    var instructions: HTMLElement;
+    var spotLight: SpotLight;
+    var groundGeometry: CubeGeometry;
+    var groundMaterial: Physijs.Material;
+    var ground: Physijs.Mesh;
+    var clock: Clock;
     var playerGeometry: CubeGeometry;
     var playerMaterial: Physijs.Material;
     var player: Physijs.Mesh;
-    var stats: Stats;
-    var spotLight: THREE.SpotLight;
-    
-    
-    function init(){
+    var sphereGeometry: SphereGeometry;
+    var sphereMaterial: Physijs.Material;
+    var sphere: Physijs.Mesh;
+    var keyboardControls: objects.KeyboardControls;
+
+    function init() {
+        //Create to HTMLElements
+        blocker = document.getElementById("blocker");
+        instructions = document.getElementById("instructions");
         
-        scene = new Scene();
+        //check to see if pointerlock is supported
+        havePointerLock = 'pointerLockElement' in document ||
+            'mozPointerLockElement' in document ||
+            'webkitPointerLockElement' in document;
+
+        if (havePointerLock) {
+            element = document.body;
+
+            instructions.addEventListener('click', () => {
+                
+                // Ask the user for pointer lock
+                console.log("Requesting PointerLock");
+
+                element.requestPointerLock = element.requestPointerLock ||
+                    element.mozRequestPointerLock ||
+                    element.webkitRequestPointerLock;
+
+                element.requestPointerLock();
+            });
+
+            document.addEventListener('pointerlockchange', pointerLockChange);
+            document.addEventListener('mozpointerlockchange', pointerLockChange);
+            document.addEventListener('webkitpointerlockchange', pointerLockChange);
+            document.addEventListener('pointerlockerror', pointerLockError);
+            document.addEventListener('mozpointerlockerror', pointerLockError);
+            document.addEventListener('webkitpointerlockerror', pointerLockError);
+        }
+
+        // Scene changes for Physijs
         scene.name = "Main";
-        scene.fog = new THREE.Fog(0x000000,0,750);
+        scene.fog = new THREE.Fog(0xffffff, 0 , 750);
+        //scene.setGravity(new THREE.Vector3(0, -10, 0));
         
         scene.addEventListener('update', () => {
            scene.simulate(undefined, 2); 
         });
         
-        playerGeometry = new BoxGeometry(2, 2, 2);
-        playerMaterial = Physijs.createMaterial(new LambertMaterial({color: 0x00ff00}), 0.4, 0);
+        // setup a THREE.JS Clock object
+        clock = new Clock();
         
-        player = new Physijs.BoxMesh(playerGeometry, playerMaterial, 1);
-        player.position.set(0, 0, 0);
-        player.receiveShadow = true;
-        player.castShadow = true;
-        player.name = "Player";
-        
-        spotLight = new THREE.SpotLight(0xffffff);
+        setupRenderer(); // setup the default renderer
+	
+        setupCamera(); // setup the camera
+
+
+        // Spot Light
+        spotLight = new SpotLight(0xffffff);
         spotLight.position.set(20, 40, -15);
         spotLight.castShadow = true;
         spotLight.intensity = 2;
@@ -65,20 +133,99 @@ var game = (() => {
         scene.add(spotLight);
         console.log("Added spotLight to scene");
         
+        // Burnt Ground
+        groundGeometry = new BoxGeometry(32, 1, 32);
+        groundMaterial = Physijs.createMaterial(new LambertMaterial({ color: 0xe75d14 }), 0.4, 0);
+        ground = new Physijs.ConvexMesh(groundGeometry, groundMaterial, 0);
+        ground.receiveShadow = true;
+        ground.name = "Ground";
+        scene.add(ground);
+        console.log("Added Burnt Ground to scene");
+ 
+        // Player Object
+        playerGeometry = new BoxGeometry(2, 2, 2);
+        playerMaterial = Physijs.createMaterial(new LambertMaterial({color: 0x00ff00}), 0.4, 0);
         
+        player = new Physijs.BoxMesh(playerGeometry, playerMaterial, 1);
+        player.position.set(0, 30, 10);
+        player.receiveShadow = true;
+        player.castShadow = true;
+        player.name = "Player";
         scene.add(player);
-        console.log("Player Added to Scene");
+        console.log("Added Player to Scene");
         
+        player.addEventListener('collision', (event) => {
+           if(event.name === "Ground") {
+               console.log("player hit the ground");
+           }
+           if(event.name === "Sphere") {
+               console.log("player hit the sphere");
+           }
+        });
+        
+        // Sphere Object
+        sphereGeometry = new SphereGeometry(2, 32, 32);
+        sphereMaterial = Physijs.createMaterial(new LambertMaterial({color: 0x00ff00}), 0.4, 0);
+        sphere = new Physijs.SphereMesh(sphereGeometry, sphereMaterial, 1);
+        sphere.position.set(0, 60, 10);
+        sphere.receiveShadow = true;
+        sphere.castShadow = true;
+        sphere.name = "Sphere";
+        scene.add(sphere);
+        console.log("Added Sphere to Scene");
+        
+        keyboardControls = new objects.KeyboardControls();
+        
+        // add controls
+        gui = new GUI();
+        control = new Control();
+        addControl(control);
+
+        // Add framerate stats
         addStatsObject();
-        setupRenderer();
-        setupCamera();
-        gameLoop();
+        console.log("Added Stats to scene...");
+
+        document.body.appendChild(renderer.domElement);
+        gameLoop(); // render the scene	
         scene.simulate();
         
+        window.addEventListener('resize', onWindowResize, false);
     }
     
+    //PointerLockChange Event Handler
+    function pointerLockChange(event): void {
+        if (document.pointerLockElement === element) {
+            // enable our mouse and keyboard controls
+            blocker.style.display = 'none';
+        } else {
+            // disable our mouse and keyboard controls
+            blocker.style.display = '-webkit-box';
+            blocker.style.display = '-moz-box';
+            blocker.style.display = 'box';
+            instructions.style.display = '';
+            console.log("PointerLock disabled");
+        }
+    }
     
-     function addStatsObject() {
+    //PointerLockError Event Handler
+    function pointerLockError(event): void {
+        instructions.style.display = '';
+        console.log("PointerLock Error Detected!!");
+    }
+    
+    // Window Resize Event Handler
+    function onWindowResize() {
+        camera.aspect = window.innerWidth / window.innerHeight;
+        camera.updateProjectionMatrix();
+        renderer.setSize(window.innerWidth, window.innerHeight);
+    }
+
+    function addControl(controlObject: Control): void {
+        /* ENTER CODE for the GUI CONTROL HERE */
+    }
+
+    // Add Frame Rate Stats to the Scene
+    function addStatsObject() {
         stats = new Stats();
         stats.setMode(0);
         stats.domElement.style.position = 'absolute';
@@ -86,11 +233,26 @@ var game = (() => {
         stats.domElement.style.top = '0px';
         document.body.appendChild(stats.domElement);
     }
-    
-    function gameLoop():void{
+
+    // Setup main game loop
+    function gameLoop(): void {
         stats.update();
         
-        console.log("Firing Game Loop");
+        if(keyboardControls.moveForward) {
+            console.log("Moving Forward");
+        }
+        if(keyboardControls.moveLeft) {
+            console.log("Moving left");
+        }
+        if(keyboardControls.moveBackward) {
+            console.log("Moving Backward");
+        }
+        if(keyboardControls.moveRight) {
+            console.log("Moving Right");
+        }
+        if(keyboardControls.jump) {
+            console.log("Jumping");
+        }
         
         // render using requestAnimationFrame
         requestAnimationFrame(gameLoop);
@@ -98,8 +260,8 @@ var game = (() => {
         // render the scene
         renderer.render(scene, camera);
     }
-    
-     // Setup default renderer
+
+    // Setup default renderer
     function setupRenderer(): void {
         renderer = new Renderer({ antialias: true });
         renderer.setClearColor(0x404040, 1.0);
@@ -109,19 +271,19 @@ var game = (() => {
         console.log("Finished setting up Renderer...");
     }
 
-    
-    function setupCamera():void{
-        camera = new PerspectiveCamera(35, config.Screen.RATIO,0.1,100);
-        camera.position.set(10,10,10);
-        camera.lookAt(new Vector3(0,0,0));
+    // Setup main camera for the scene
+    function setupCamera(): void {
+        camera = new PerspectiveCamera(35, config.Screen.RATIO, 0.1, 100);
+        camera.position.set(0, 10, 30);
+        camera.lookAt(new Vector3(0, 0, 0));
         console.log("Finished setting up Camera...");
-        console.log(CScreen.WIDTH + "");
     }
-    
+
     window.onload = init;
 
     return {
         scene: scene
     }
-    
+
 })();
+
